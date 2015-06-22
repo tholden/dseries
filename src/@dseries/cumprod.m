0f6faa29b7aa449041f705abfb017b8c0cda0f68
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2015-06-22 15:40:35 +0200

    Changed unit test for cumprod and cumsum methods.
    Do not test the equality of dseries object but the equality of data
    members instead.

--- src/@dseries/cumprod.m
+++ src/@dseries/cumprod.m
@@ -201,9 +201,7 @@ end
 %$ ts4 = dseries([.25; .5; 1; 2; 4; 8; 16]*pi, [], A_name, []);
 %$
 %$ % Check the results.
-%$ warning off, % Because the names of the variables are not the same...
-%$ t(1) = dassert(ts3,ts4);
-%$ warning_config
+%$ t(1) = dassert(ts3.data,ts4.data);
 %$ T = all(t);
 %@eof:4
 
@@ -225,8 +223,6 @@ end
 %$ ts4 = dseries([.25; .5; 1; 2; 4; 8; 16]*pi, [], A_name, []);
 %$
 %$ % Check the results.
-%$ warning off, % Because the names of the variables are not the same...
-%$ t(1) = dassert(ts3,ts4);
-%$ warning_config
+%$ t(1) = dassert(ts3.data,ts4.data);
 %$ T = all(t);
 %@eof:5
