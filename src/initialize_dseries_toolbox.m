744c3e9f9b88b1442f948026ed714ede02315be2
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2015-11-26 22:30:43 +0100

    Added missing function.

--- src/initialize_dseries_toolbox.m
+++ src/initialize_dseries_toolbox.m
@@ -36,6 +36,10 @@ if ~exist('sample_hp_filter','file')
     addpath([dseries_src_root '/utilities/missing/sample_hp_filter'])
 end
 
+if ~exist('get_file_extension','file')
+    addpath([dseries_src_root '/utilities/missing/get_file_extension'])
+end
+
 if isoctave && ~exist('user_has_octave_forge_package','file')
     addpath([dseries_src_root '/utilities/missing/user_has_octave_forge_package'])
 end
